6c1cd9827f4abfe408fcce0a580cf1cc784ea976
Nicolas Patry <patry.nicolas@protonmail.com>
2019-07-05 18:24:04 +0200

    Adding a test to remember all passing solutions (only the smallest possible example).

---
+++ test.py
@@ -0,0 +1,59 @@
+import unittest
+import logging
+from core import create_models, logdir, FutureReturnBaseline, solve, logger
+from vanilla import VPGUpdate
+from ppo import PPO
+
+
+logger.setLevel(logging.CRITICAL)
+
+
+class TestVanilla(unittest.TestCase):
+    hidden_sizes = [100]
+    lr = 1e-2
+
+    def test_cartpole_v0(self):
+        env_name = "CartPole-v0"
+        env, policy, optimizer = create_models(env_name, self.hidden_sizes, self.lr)
+        baseline = FutureReturnBaseline()
+        policy_update = VPGUpdate(policy, optimizer, baseline)
+        result = solve(env_name, env, policy_update, logdir)
+        self.assertEqual(result, True)
+
+    def test_cartpole_v1(self):
+        env_name = "CartPole-v1"
+        env, policy, optimizer = create_models(env_name, self.hidden_sizes, self.lr)
+        baseline = FutureReturnBaseline()
+        policy_update = VPGUpdate(policy, optimizer, baseline)
+        result = solve(env_name, env, policy_update, logdir)
+        self.assertEqual(result, True)
+
+    def test_inverted_pendulum_v2(self):
+        env_name = "InvertedPendulum-v2"
+        env, policy, optimizer = create_models(env_name, self.hidden_sizes, self.lr)
+        baseline = FutureReturnBaseline()
+        policy_update = VPGUpdate(policy, optimizer, baseline)
+        result = solve(env_name, env, policy_update, logdir)
+        self.assertEqual(result, True)
+
+
+class TestPpo(unittest.TestCase):
+    hidden_sizes = [100]
+    lr = 1e-2
+
+    def test_lunar_lander_v2(self):
+        env_name = "LunarLander-v2"
+        env, policy, optimizer = create_models(env_name, self.hidden_sizes, self.lr)
+        baseline = FullReturnBaseline()
+        policy_iters = 10
+        target_kl = 0.015
+        clip_ratio = 0.2
+        policy_update = PPO(
+            policy_iters, clip_ratio, target_kl, policy, optimizer, baseline
+        )
+        result = solve(env_name, env, policy_update, logdir)
+        self.assertEqual(result, True)
+
+
+if __name__ == "__main__":
+    unittest.main()
